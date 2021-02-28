4362a0bf86a639420c3434f1703d673e9eaa3eaa
brenanwanderley <brenanwanderley@gmail.com>
2021-02-27 23:23:32 -0300

    add menuBar

---
+++ src/Components/MenuBar.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import './MenuBar.css';
+
+function MenuBar(){
+    return (
+        <div className='menu-bar'>
+            <a>Home</a>
+            <a>News</a>
+            <a>Info</a>
+            <a>Contact</a>
+        </div>
+    )
+}
+
+export default MenuBar;
